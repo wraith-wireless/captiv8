d222f325e34dc70b65aac65ba60294756dd72ed9
WraithWireless <IntoTh3Dark>
2016-08-05 10:45:57 -0600

    continue on Collector

--- captiv8/collect.py
+++ captiv8/collect.py
@@ -20,7 +20,7 @@ modifications, are permitted provided that the following conditions are met:
     any contributors may be used to endorse or promote products derived from
     this software without specific prior written permission.
 
-Provides the Collector class
+Provides the Collector and Tuner class
 
 """
 
@@ -34,8 +34,27 @@ __email__ = 'wraith.wireless@yandex.com'
 __status__ = 'Development'
 
 import multiprocessing as mp
+import threading
+import time
 import pyric
 import pyric.pyw as pyw
+import pyric.lib.libnl as nl
+import pyric.utils.channels as channels
+
+class Tuner(threading.Thread):
+    """ tunes radio """
+    def __init__(self,card,scan):
+        """
+         tunes card
+         :param card: the card/radio to tune
+         :param scan: scan list of available channels, a list of tuples
+          t = (rf, channel width)
+        """
+        threading.Thread.__init__(self)
+        self._card = card
+        self._scan = scan
+        self._i = 0
+        self._nlsock = None
 
 # noinspection PyCallByClass
 class Collector(mp.Process):
@@ -74,16 +93,41 @@ class Collector(mp.Process):
     def _setup(self):
         """ setup radio and tuning thread """
         # set up the radio for collection
+        nlsock = None
         try:
+            # get a netlink socket for this
+            nlsock = nl.nl_socket_alloc()
+
             # store the old card and create a new one, deleting any assoc interfaces
-            self._oinfo = pyw.devinfo(self._dev)
+            self._oinfo = pyw.devinfo(self._dev,nlsock)
             self._ocard = self._oinfo['card']
-            self._ncard = pyw.devadd(self._ocard,'capt0','monitor')
-            for card, _ in pyw.ifaces(self._ncard):
-                if card.dev != self._ncard.dev: pyw.devdel(card)
+            self._ncard = pyw.devadd(self._ocard,'cap8','monitor',None,nlsock)
+            for card, _ in pyw.ifaces(self._ncard,nlsock):
+                if card.dev != self._ncard.dev: pyw.devdel(card,nlsock)
             if not pyw.isup(self._ncard): pyw.up(self._ncard)
-        except pyric.error as e:
+
+            # determine scannable channels
+            scan = []
+            for rf in pyw.devfreqs(self._ncard,nlsock):
+                for chw in channels.CHTYPES:
+                    try:
+                        pyw.freqset(self._ncard,rf,chw,nlsock)
+                    except pyric.error:
+                        pass
+                    else:
+                        scan.append((rf, chw))
+            assert(scan)
+        except AssertionError:
+            self._teardown()
+            raise RuntimeError("No valid scan channels found")
+        except nl.error as e:
+            self._teardown()
             raise RuntimeError("ERRNO {0} {1}".format(e.errno, e.strerror))
+        except pyric.error as e:
+            self._teardown() # attempt to restore
+            raise RuntimeError("ERRNO {0} {1}".format(e.errno, e.strerror))
+        finally:
+            nl.nl_socket_free(nlsock)
 
     def _teardown(self):
         """ restore radio and wait on tuning thread"""
